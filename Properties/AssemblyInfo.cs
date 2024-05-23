21dc921013935563bc95aae434fe69177efcda27
UlvakSkillz <156770480+UlvakSkillz@users.noreply.github.com>
2024-05-22 17:42:54 -0700

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,44 @@
+﻿using HandDistanceRecorder; // The namespace of your mod class
+using MelonLoader;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+// ...
+[assembly: MelonInfo(typeof(main), "HandDistanceRecorder", "1.0.0", "UlvakSkillz")]
+[assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
+[assembly: MelonColor(ConsoleColor.Magenta)]
+[assembly: MelonAuthorColor(ConsoleColor.Magenta)]
+[assembly: VerifyLoaderVersion(0, 5, 7)]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HandDistanceRecorder")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HandDistanceRecorder")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b14d69ae-fbeb-434d-bea3-7c74f1b85b50")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ main.cs
@@ -0,0 +1,80 @@
+﻿using MelonLoader;
+using UnityEngine;
+using RumbleModUI;
+using System.Collections;
+using RUMBLE.Managers;
+using System;
+
+namespace HandDistanceRecorder
+{
+    public class main : MelonMod
+    {
+        private bool init = false;
+        private Vector3 lastHandPositionL, lastHandPositionR;
+        private Transform handLTransform, handRTransform;
+        private float distanceL = 0;
+        private float distanceR = 0;
+        UI UI = UI.instance;
+        private Mod HandDistanceRecorder = new Mod();
+        private DateTime waitTill;
+
+        public override void OnLateInitializeMelon()
+        {
+            HandDistanceRecorder.ModName = "HandDistanceRecorder";
+            HandDistanceRecorder.ModVersion = "1.0.0";
+            HandDistanceRecorder.SetFolder("HandDistanceRecorder");
+            HandDistanceRecorder.AddToList("Description", ModSetting.AvailableTypes.Description, "", "Hand Distances Recorded:" + Environment.NewLine + "Left: 0" + Environment.NewLine + "Right: 0");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            init = false;
+            waitTill = DateTime.Now.AddSeconds(1);
+            if (sceneName != "Loader") { MelonCoroutines.Start(SetInitialHandPositions()); }
+        }
+
+        public override void OnUpdate()
+        {
+            if (!init) { return; }
+            if (DateTime.Now < waitTill)
+            {
+                lastHandPositionL = handLTransform.position;
+                lastHandPositionR = handRTransform.position;
+                return;
+            }
+            distanceL += Vector3.Distance(lastHandPositionL, handLTransform.position);
+            distanceR += Vector3.Distance(lastHandPositionR, handRTransform.position);
+            lastHandPositionL = handLTransform.position;
+            lastHandPositionR = handRTransform.position;
+            HandDistanceRecorder.Settings[0].Description = "Hand Distances Recorded:" + Environment.NewLine + "Left: " + distanceL.ToString("0.###") + " Units" + Environment.NewLine + "Right: " + distanceR.ToString("0.###") + " Units";
+        }
+
+        public override void OnFixedUpdate()
+        {
+            if (UI.GetInit() && !HandDistanceRecorder.GetUIStatus()) { UI.AddMod(HandDistanceRecorder); }
+        }
+
+        public IEnumerator SetInitialHandPositions()
+        {
+            bool gotHands = false;
+            while (!gotHands)
+            {
+                try
+                {
+                    GameObject VR = PlayerManager.instance.localPlayer.Controller.gameObject.transform.GetChild(1).gameObject;
+                    handLTransform = VR.transform.GetChild(1);
+                    handRTransform = VR.transform.GetChild(2);
+                    lastHandPositionL = handLTransform.localPosition;
+                    lastHandPositionR = handRTransform.localPosition;
+                    if (handLTransform.position != null && handRTransform.position != null) { gotHands = true; }
+                }
+                catch { }
+                if (!gotHands)
+                {
+                    yield return new WaitForFixedUpdate();
+                }
+            }
+            init = true;
+        }
+    }
+}
